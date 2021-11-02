9376093e5271e09fcf84b3b9a98792ae8bc3dd7a
Vincent Agnano <vinyll@protonmail.com>
2021-11-02 17:32:41 +0100

    integrate specific design

--- widget.js
+++ widget.js
@@ -1,15 +1,20 @@
-class FeedbackWidget extends HTMLElement {
+class WebComponent extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       ${this.html}
-      <style scope>${this.style}</style>
+      <style>${this.style}</style>
     `
   }
+}
 
+
+class FeedbackWidget extends WebComponent {
   get html() {
     return `
     <section>
-      Donner mon avis
+      <button title="Trouver une assistance et donner mon avis">
+        Que cherchez-vous ?
+      </button>
     </section>
     `
   }
@@ -19,16 +24,27 @@ class FeedbackWidget extends HTMLElement {
     feedback-widget {
       display: inline-block;
       position: fixed;
-      right: -6rem;
-      bottom: 10rem;
-      background-color: yellow;
-      color: red;
-      padding: 1rem;
-      font-size: 2rem;
-      transform: rotate(-90deg);
+      right: 56px;
+      bottom: 48px;
+    }
+
+    feedback-widget button {
+      border: 0;
+      cursor: pointer;
+      background: #000091;
+      border-radius: 5px;
+      color: white;
+      padding: 4px 16px;
+      font-size: 14px;
+      line-height: 24px;
+      text-align: center;
+      font-family: Marianne, Arianne, Verdana;
+      font-style: normal;
+      font-weight: bold;
     }
     `
   }
 }
+
 
 customElements.define('feedback-widget', FeedbackWidget)
